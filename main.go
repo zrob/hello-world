75fe0c6b6160ab710a7de36f5650d3fe8c52fe25
Zach Robinson <zach.robinson@gmail.com>
2022-08-04 18:46:20 -0700

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var addr = flag.String("addr", ":8080", "addr to bind to")
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
 
-	fmt.Fprintf(w, "hello world\n")
+	fmt.Fprintf(w, "explore world\n")
 }
 
 func main() {
